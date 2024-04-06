eeb012d57722b5ada9404380d84375437582d07d
Samuel Burger <xburger2@fi.muni.cz>
2024-04-06 12:25:22 +0200

    finised basic functionaility

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    default,
     env::args,
     fs::File,
     io::{self, Read, Write},
@@ -10,22 +11,41 @@ enum Output {
     Bin,
     Dec,
     Str,
-    HexPlusStr
+    HexPlusStr,
 }
-struct Mode {
+struct Profile {
     out: Output,
     line_l: u32,
-    raw: bool
+    raw: bool,
 }
 
 fn main() {
+    let mut profile = basic_profile();
     let args = args().skip(1);
     for a in args {
-        hex_dump(a);
+        if a.as_str().starts_with("-") {
+            match a.as_str() {
+                "-h" | "--help" => show_help(),
+                "-H" => profile.out = Output::Hex,
+                "-O" => profile.out = Output::Oct,
+                "-B" => profile.out = Output::Bin,
+                "-D" => profile.out = Output::Dec,
+                "-S" => profile.out = Output::Str,
+                "-H+" => profile.out = Output::HexPlusStr,
+                "-r" => profile.raw = true,
+                _ => {
+                    println!("Illegal option: {}", a);
+                    show_help();
+                }
+            }
+        } else {
+            hex_dump(&profile, a);
+        }
+
     }
 }
 
-fn hex_dump(str: String) {
+fn hex_dump(profile:&Profile, str: String) {
     let path = PathBuf::from(str);
     let file = match File::open(path) {
         Ok(file) => file,
@@ -41,3 +61,15 @@ fn hex_dump(str: String) {
     }
     print!("\n");
 }
+
+fn basic_profile() -> Profile {
+    let output: Output = Output::Hex;
+    let profile = Profile {
+        out: output,
+        line_l: 32,
+        raw: false,
+    };
+    profile
+}
+
+fn show_help() {}
